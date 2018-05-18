be67f08c51040e6e1869b3a4fc9bad247b8e14f4
mleonardo93 <mleonardo@gmail.com>
2018-05-17 22:22:29 -0400

    checkpoint 17 work

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,11 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
+    for post in @posts do
+      if (((post.id - 1)% 5) == 0) || post.id == 1
+        post.title = "SPAM"
+      end
+    end
   end
 
   def show
