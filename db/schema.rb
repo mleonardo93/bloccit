3e6bfb507ed485d1b7571e29d43fcf7351c98771
mleonardo93 <mleonardo@gmail.com>
2018-05-18 22:21:48 -0400

    debugging commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_15_023814) do
+ActiveRecord::Schema.define(version: 2018_05_19_014331) do
+
+  create_table "advertisements", force: :cascade do |t|
+    t.string "title"
+    t.text "copy"
+    t.integer "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "answers", force: :cascade do |t|
     t.text "body"
