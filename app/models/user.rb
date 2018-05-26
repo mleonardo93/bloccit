1face77af023ffffe8022fbb227af75da68eb5e9
mleonardo93 <mleonardo@gmail.com>
2018-05-26 16:24:04 -0400

    checkpoint 23 assignment

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   before_save { self.email = email.downcase if email.present? }
+  before_save { self.name = name.split(" ").each {|word| word.capitalize!}.join(" ") if !name.nil? }
 
   validates :name, length: { minimum: 1, maximum: 100 }, presence: true
   # This differs from how it's written in the checkpoint because I was getting an error. 
