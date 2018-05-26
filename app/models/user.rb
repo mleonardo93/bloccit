10f7f16ae306e2f380e1129626afacb967400ae7
mleonardo93 <mleonardo@gmail.com>
2018-05-26 16:04:32 -0400

    checkpoint 23 work

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+  before_save { self.email = email.downcase if email.present? }
+
+  validates :name, length: { minimum: 1, maximum: 100 }, presence: true
+  # This differs from how it's written in the checkpoint because I was getting an error. 
+  # I'm still not sure where the error comes from, but I've asked in Slack.
+  if "password_digest.nil?"
+    validates :password, presence: true, length: { minimum: 6 }
+  end
+  validates :password, length: { minimum: 6 }, allow_blank: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 254 }
+  has_secure_password
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     resources :posts, except: [:index]
     resources :sponsored_posts
   end
+  resources :users, only: [:new, :create]
   resources :advertisements
   resources :questions
   get "about" => "welcome#about"
