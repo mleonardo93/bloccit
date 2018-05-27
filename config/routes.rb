3347a39ccc7374f14da2f984a34d48f32e07ecb9
mleonardo93 <mleonardo@gmail.com>
2018-05-26 23:36:11 -0400

    checkpoint 24 assignment

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   get "about" => "welcome#about"
   get 'welcome/contact'
   get 'welcome/faq'
+  post 'users/confirm' => 'users#confirm'
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
