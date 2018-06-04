b5bd8d944eb8be77debc91220c6f718dc5818e17
mleonardo93 <mleonardo@gmail.com>
2018-06-04 18:58:23 -0400

    checkpoint 27 assignment

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,8 @@
 class PostsController < ApplicationController
 
   before_action :require_sign_in, except: :show
-  before_action :authorize_user, except: [:show, :new, :create]
+  before_action :authorize_moderator, except: [:show, :new, :create]
+  before_action :authorize_admin, except: [:show, :new, :create, :edit, :update]
 
   def show
     @post = Post.find(params[:id])
@@ -60,7 +61,15 @@ class PostsController < ApplicationController
     params.require(:post).permit(:title, :body)
   end
 
-  def authorize_user
+  def authorize_moderator
+    post = Post.find(params[:id])
+    unless current_user == post.user || current_user.admin? || current_user.moderator?
+      flash[:alert] = "You must have more privileges to do that."
+      redirect_to [post.topic, post]
+    end
+  end
+
+  def authorize_admin
     post = Post.find(params[:id])
     unless current_user == post.user || current_user.admin?
       flash[:alert] = "You must be an admin to do that."
--- app/models/user.rb
+++ app/models/user.rb
@@ -14,5 +14,5 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 254 }
   has_secure_password
 
-  enum role: [:member, :admin]
+  enum role: [:member, :moderator, :admin]
 end
