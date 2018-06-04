87663834d1f40b0987467f2a76848359dd734290
mleonardo93 <mleonardo@gmail.com>
2018-06-03 22:05:53 -0400

    checkpoint 27 debugging commit 2

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,15 +2,14 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   
   before_save { self.email = email.downcase if email.present? }
-  before_save { self.name = name.split(" ").each{|word| word.capitalize!}.join(" ") if !name.nil? }
+  before_save { self.name = name.split(" ").each{|word| word.capitalize!}.join(" ") if name }
   before_save { self.role ||= :member }
 
   validates :name, length: { minimum: 1, maximum: 100 }, presence: true
   # This differs from how it's written in the checkpoint because I was getting an error. 
   # I'm still not sure where the error comes from, but I've asked in Slack.
-  if "self.password_digest.nil = true"
-    validates :password, presence: true, length: { minimum: 6 }
-  end
+  validates :password, presence: true, length: { minimum: 6 } if :password_digest.nil?
+  
   validates :password, length: { minimum: 6 }, allow_blank: true
   validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 254 }
   has_secure_password
--- db/seeds.rb
+++ db/seeds.rb
@@ -62,9 +62,16 @@ posts = Post.all
     )
 end
 
-user = User.first
-user.update_attributes!(
-    email: "mleonardo@gmail.com",
+admin = User.create!(
+    name: "Admin User"
+    email: "admin@example.com",
+    password: "helloworld"
+    role: "admin"
+)
+
+member = User.create!(
+    name: "Member User"
+    email: "member@example.com",
     password: "helloworld"
 )
 
