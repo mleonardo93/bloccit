b547df4efa29f963097fddf10258dbca5122b98c
mleonardo93 <mleonardo@gmail.com>
2018-06-04 22:37:32 -0400

    checkpoint 28 work

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
     resources :posts, except: [:index]
     resources :sponsored_posts
   end
+  resources :posts, only: [] do
+    resources :comments, only: [:create, :destroy]
+  end
   resources :users, only: [:new, :create]
   resources :sessions, only: [:new, :create, :destroy]
   resources :advertisements
