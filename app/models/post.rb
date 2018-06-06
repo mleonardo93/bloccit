e016c6b5e2419cb8e01cf2f4e01aa99e2daae29d
mleonardo93 <mleonardo@gmail.com>
2018-06-05 20:01:35 -0400

    checkpoint 29 work

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,14 +1,33 @@
 class Post < ApplicationRecord
-    belongs_to :topic
-    belongs_to :user
-    has_many :comments, dependent: :destroy
+  belongs_to :topic
+  belongs_to :user
+  has_many :comments, dependent: :destroy
+  has_many :votes, dependent: :destroy
 
-    default_scope { order("created_at DESC") }
-    scope :ordered_by_title, -> { Post.order("posts.title") }
-    scope :ordered_by_reverse_created_at, -> { Post.order("posts.created_at").reverse }
+  default_scope { order("rank DESC") }
+  scope :ordered_by_title, -> { Post.order("posts.title") }
+  scope :ordered_by_reverse_created_at, -> { Post.order("posts.created_at").reverse }
 
-    validates :title, length: { minimum: 5 }, presence: true
-    validates :body, length: { minimum: 20 }, presence: true
-    validates :topic, presence: true
-    validates :user, presence: true
+  validates :title, length: { minimum: 5 }, presence: true
+  validates :body, length: { minimum: 20 }, presence: true
+  validates :topic, presence: true
+  validates :user, presence: true
+
+  def up_votes
+    votes.where(value: 1).count
+  end
+
+  def down_votes
+    votes.where(value: -1).count
+  end
+
+  def points
+    votes.sum(:value)
+  end
+
+  def update_rank
+    age_in_days = (created_at - Time.new(1970,1,1)) / 1.day.seconds
+    new_rank = points + age_in_days
+    update_attribute(:rank, new_rank)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,86 +1,88 @@
 require 'random_data'
 
 5.times do 
-    User.create!(
-        name: RandomData.random_name,
-        email: RandomData.random_email,
-        password: RandomData.random_sentence
-    )
+  User.create!(
+    name: RandomData.random_name,
+    email: RandomData.random_email,
+    password: RandomData.random_sentence
+  )
 end
 users = User.all
 
 15.times do
-    Topic.create!(
-        name: RandomData.random_sentence,
-        description: RandomData.random_paragraph
-    )
+  Topic.create!(
+    name: RandomData.random_sentence,
+    description: RandomData.random_paragraph
+  )
 end
 
 topics = Topic.all
 
 50.times do
-    Post.create!(
-        user: users.sample,
-        topic: topics.sample,
-        title: RandomData.random_sentence,
-        body: RandomData.random_paragraph
-    )
+  post = Post.create!(
+    user: users.sample,
+    topic: topics.sample,
+    title: RandomData.random_sentence,
+    body: RandomData.random_paragraph
+  )
+  post.update_attribute(:created_at, rand(10.minutes .. 1.year).ago)
+  rand(1..5).times { post.votes.create!(value: [-1, 1].sample, user: users.sample) }
 end
 
 10.times do
-    SponsoredPost.create!(
-        topic: topics.sample,
-        title: RandomData.random_sentence,
-        body: RandomData.random_paragraph,
-        price: rand(1000)
-    )
+  SponsoredPost.create!(
+    topic: topics.sample,
+    title: RandomData.random_sentence,
+    body: RandomData.random_paragraph,
+    price: rand(1000)
+  )
 end
 
 
 50.times do
-    Advertisement.create!(
-        title: RandomData.random_sentence,
-        copy: RandomData.random_paragraph,
-        price: rand(1000)
-    )
+  Advertisement.create!(
+    title: RandomData.random_sentence,
+    copy: RandomData.random_paragraph,
+    price: rand(1000)
+  )
 end
 
 50.times do
-    Question.create!(
-        title: RandomData.random_sentence,
-        body: RandomData.random_paragraph,
-        resolved: false
-    )
+  Question.create!(
+    title: RandomData.random_sentence,
+    body: RandomData.random_paragraph,
+    resolved: false
+  )
 end
 
 posts = Post.all
 
 100.times do
-    Comment.create!(
-        user: users.sample,
-        post: posts.sample,
-        body: RandomData.random_paragraph
-    )
+  Comment.create!(
+    user: users.sample,
+    post: posts.sample,
+    body: RandomData.random_paragraph
+  )
 end
 
 admin = User.create!(
-    name: "Admin User",
-    email: "admin@example.com",
-    password: "helloworld",
-    role: "admin"
+  name: "Admin User",
+  email: "admin@example.com",
+  password: "helloworld",
+  role: "admin"
 )
 
 moderator = User.create!(
-    name: "Moderator User",
-    email: "moderator@example.com",
-    password: "helloworld",
-    role: "moderator"
+  name: "Moderator User",
+  email: "moderator@example.com",
+  password: "helloworld",
+  role: "moderator"
 )
 
 member = User.create!(
-    name: "Member User",
-    email: "member@example.com",
-    password: "helloworld"
+  name: "Member User",
+  email: "member@example.com",
+  password: "helloworld"
 )
 
 puts "Seed finished"
@@ -89,5 +91,6 @@ puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{SponsoredPost.count} sponsored posts created"
 puts "#{Comment.count} comments created"
+puts "#{Vote.count} votes created"
 puts "#{Advertisement.count} ads created"
 puts "#{Question.count} questions created"
