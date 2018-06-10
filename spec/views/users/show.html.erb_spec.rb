f481e2d813a2c0f846a102df5a439c0bd2440946
mleonardo93 <mleonardo@gmail.com>
2018-06-09 21:22:32 -0400

    checkpoint 31 assignment

--- spec/views/users/show.html.erb_spec.rb
+++ spec/views/users/show.html.erb_spec.rb
@@ -1,14 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe "users/show.html.erb", type: :view do
-  let(:user) { create(:user) }
-  let(:other_user) { create(:user) }
-  let(:post) { create(:post) }
-  let(:other_post) { create(:post, user: other_user) }
-  let(:favorite) { Favorite.create!(post: post, user: user) }
 
   it "displays the post" do
+    u = create(:user)
+    p = create(:post, user: u, title: "Post title")
+
+    assign(:user, u)
     render
-    rendered.should contain(post)
+    expect(view).to render_template(partial: "_post", count: 1)
+  end
+
+  it "displays comments" do
+    u = create(:user)
+    p = create(:post, user: u, title: "Post title")
+    c = create(:comment, user: u, post: p, body: "Comment body comment body comment body")
+
+    assign(:user, u)
+    render
+    expect(view).to render_template(partial: "_comment", count: 1)
+  end
+
+  it "displays favorites" do 
+    u = create(:user)
+    o_u = create(:user)
+    o_p = create(:post, user: o_u, title: "Another post title")
+    f = Favorite.create!(post: o_p, user: u) 
+
+    assign(:user, u)
+    render
+    expect(view).to render_template(partial: "_post", count: 1)
   end
 end
